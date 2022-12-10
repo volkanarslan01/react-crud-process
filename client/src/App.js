cd9b2ca951f3cf3e12b423860f4e2fd546cdbabf
volkanarslan01 <101024530+volkanarslan01@users.noreply.github.com>
2022-12-10 09:54:45 +0300

    react app visual interface

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,14 @@
 function App() {
   return (
     <div className="App">
-      <h1>CRUD APP</h1>
+      <div className="form">
+        <h1>CRUD APPLICATION</h1>
+        <label>Movie Name: </label>
+        <input type="text" name="movieName"></input>
+        <label>Review: </label>
+        <input type="text" name="review"></input>
+        <button>Sumbit</button>
+      </div>
     </div>
   );
 }
